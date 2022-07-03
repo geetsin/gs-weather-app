36d44e5c3d6d3c7b129d06fd34b9aeac15d4f612
Geet Sebastian <geetgonnarock@gmail.com>
2022-07-03 14:40:41 +1000

    Added SwaggerUI, API Rate limiting, Caching

--- weather-app/Program.cs
+++ weather-app/Program.cs
@@ -1,18 +1,81 @@
 using weather_app.Services;
 using Azure.Identity;
+using AspNetCoreRateLimit;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+if (builder.Environment.IsDevelopment())
+{
+    // Add Run-time compilation
+    builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+}
+
+//Add In-Memory caching
+builder.Services.AddMemoryCache();
 
 // Add IWeatherService Service
 builder.Services.AddTransient<IWeatherService, WeatherService>();
 
+// Add ICityService Service
+builder.Services.AddTransient<ICityService, CityService>();
+
+// API Rate Limiting
+builder.Services.AddMemoryCache();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddInMemoryRateLimiting();
+// Configure Rate Limiting
+builder.Services.Configure<IpRateLimitOptions>(options =>
+{
+    options.EnableEndpointRateLimiting = true;
+    options.StackBlockedRequests = true;
+    options.HttpStatusCode = 429;
+    options.RealIpHeader = "X-Real-IP";
+    options.GeneralRules = new List<RateLimitRule>
+    {
+        new RateLimitRule
+        {
+            Endpoint = "*",
+            Period = "30d",
+            Limit = 500,
+        },
+        new RateLimitRule
+        {
+            Endpoint = "*",
+            Period = "10s",
+            Limit = 5,
+        }
+    };
+});
+
+//Configure Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "City API", 
+        Version = "v1",
+        Description = "An API to get data on world cities",
+        Contact = new OpenApiContact
+        {
+            Name = "Geet Sebastian",
+            Email = "hello@geets.in",
+            Url = new Uri("https://geets.in/"),
+        },
+    });
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+
+
 //Connecting to Azure Key Vault in Production
-if(builder.Environment.IsProduction())
+if (builder.Environment.IsProduction())
 {
     builder.Configuration.AddAzureKeyVault(
         new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
@@ -20,6 +83,13 @@ if(builder.Environment.IsProduction())
 }
 
 var app = builder.Build();
+
+app.UseIpRateLimiting();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "City API V1");
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -36,8 +106,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}/{id?}");
+
+});
+
 
 app.Run();
